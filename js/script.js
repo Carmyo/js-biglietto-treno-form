dc3da34dcdd93470de3a411bafffdea45296874b
Carmyo <carmelodicristina@gmail.com>
2022-08-29 19:04:09 +0200

    done

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
-const distance = prompt('Quanta distanza vuoi percorrere?');
-const age = prompt('Quanti anni hai?');
-let earlyprice = distance * 0.21 + ' €';
-console.log(earlyprice);
-let discount = 0
 
-if (18 > (age)) {
-    discount = (earlyprice / 100 * 20);
-    console.log(earlyprice / 100 * 20) + ' €';
-}
-if (65 < (age)) {
-    discount = (earlyprice / 100 * 40);
-    console.log(earlyprice / 100 * 40 + ' €');
-}
+
+document.getElementById('genera').addEventListener("click", function () {
+
+    const age = document.getElementById('age').value;
+    // se minorenne x altrimenti y
+
+    const distance = document.getElementById('distance').value;
+
+
+    const earlyprice = distance * 0.21;
+
+    let discount = 0;
+    if (age == 'Minorenne') {
+        discount = (earlyprice / 100 * 20);
+    }
+
+    if (age == 'Senior') {
+        discount = (earlyprice / 100 * 40);
+    };
+
+    console.log(discount);
+    console.log(distance);
+    const finalprice = earlyprice - discount;
+
+    document.getElementById('price').innerHTML = new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR", }).format(finalprice);
+})
